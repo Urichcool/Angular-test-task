77df16f1b1210cee92a033ecd57527e668e39d62
Yurii Novikov <urichcoolx@gmail.com>
2024-09-25 17:16:45 +0200

    created-project

---
+++ src/app/table/table-component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+@Component({
+  imports: [MatSlideToggleModule],
+})
+class AppComponent {}
