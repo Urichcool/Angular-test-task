5314bdee94176be14a97bd55eeff6b4ab81a656e
Yurii Novikov <urichcoolx@gmail.com>
2024-09-27 01:42:07 +0200

    created-profile-card-component

--- src/app/table/table-component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-
-@Component({
-  imports: [MatSlideToggleModule],
-})
-class AppComponent {}
